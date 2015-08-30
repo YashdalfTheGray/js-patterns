5cbba2757eef137be80970e8bac65c8c065de822
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-29 20:09:22 -0500

    added the basics of the observer pattern to the controller

--- public/patterns/observerPattern/observerPattern.js
+++ public/patterns/observerPattern/observerPattern.js
@@ -5,6 +5,56 @@ angular.module('jsPatternsDemo')
     [
         function() {
             var vm = this;
+
+            function ObserverList() {
+                this.observerList = [];
+            }
+
+            ObserverList.prototype.add = function(obj) {
+                return this.observerList.push(obj);
+            };
+
+            ObserverList.prototype.count = function() {
+                return this.observerList.length;
+            };
+
+            ObserverList.prototype.get = function(index) {
+                if(index > -1 && index < this.observerList.length) {
+                    return this.observerList[index];
+                }
+            };
+
+            ObserverList.prototype.indexOf = function(obj, startIndex) {
+                return _.indexOf(this.observerList, obj, startIndex);
+            };
+
+            ObserverList.prototype.removeAt = function(index) {
+                this.observerList.splice(index, 1);
+            };
+
+            function Subject() {
+                this.observers = new ObserverList();
+            }
+
+            Subject.prototype.addObserver = function(observer) {
+                this.observers.add(observer);
+            };
+
+            Subject.prototype.removeObserver = function(observer) {
+                this.observers.removeAt(this.observers.indexOf(observer, 0));
+            };
+
+            Subject.prototype.notify = function(context) {
+                _.forEach(this.observers, function(item) {
+                    item.update(context);
+                });
+            };
+
+            function Observer() {
+                this.update = function(context) {
+                    // this will be overwritten later on
+                }
+            }
         }
     ]
 );
