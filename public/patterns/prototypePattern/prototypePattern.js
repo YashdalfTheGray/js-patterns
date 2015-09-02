e57d707d64e6cd677afe011a4b38ba6af295fe3b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-01 19:30:24 -0500

    built out the prototype pattern page and controller

--- public/patterns/prototypePattern/prototypePattern.js
+++ public/patterns/prototypePattern/prototypePattern.js
@@ -3,8 +3,42 @@
 angular.module('jsPatternsDemo')
 .controller('PrototypePatternCtrl', 
     [
-        function() {
+        'mdClearInput',
+        function(mdClearInput) {
             var vm = this;
+            vm.cars = [];
+
+            vm.car = {
+                details: function() {
+                    if (this.year && this.make && this.model) {
+                        return 'This car is a ' + this.year + ' ' + this.make + ' ' + this.model + '.';
+                    }
+                    else {
+                        return 'This is a generic car.';
+                    }
+                }
+            }
+
+            vm.cars.push(vm.car);
+
+            vm.addCar = function(year, make, model) {
+                var newCar = Object.create(vm.car, {
+                    "year": {
+                        value: year,
+                        enumerable: true
+                    },
+                    "make": {
+                        value: make,
+                        enumerable: true
+                    },
+                    "model": {
+                        value: model,
+                        enumerable: true
+                    }
+                });
+                vm.cars.push(newCar);
+                mdClearInput.clearInputBoxes(['year-input', 'make-input', 'model-input']);
+            };
         }
     ]
 );
