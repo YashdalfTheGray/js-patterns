8b8f0ad228309c45f84878fbeac72b748bf253a8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-01 19:29:59 -0500

    created a service that clears md-input-containers

--- public/app.js
+++ public/app.js
@@ -55,6 +55,37 @@ angular.module('jsPatternsDemo',
                 .warnPalette('red');
         }
 ])
+.factory('mdClearInput', 
+    [
+        function() {
+
+            function clearInput(inputId) {
+                var inputBox = document.getElementById(inputId);
+                if (inputBox !== null) {
+                    inputBox.value = '';
+                    angular.element(document.getElementById(inputId + '-container')).removeClass('md-input-has-value');
+                }
+            }
+
+            function clearInputs(inputIdArray) {
+                if (_.isArray(inputIdArray)) {
+                    _.forEach(inputIdArray, function(item) {
+                        clearInput(item);
+                    });
+                }
+                else if (_.isString(inputIdArray)) {
+                    clearInput(inputIdArray)
+                }
+            }
+
+            return {
+                clearInputBox: clearInput,
+                clearInputBoxes: clearInputs
+
+            };
+        }
+    ]
+)
 .controller('SidebarCtrl', 
     [
         '$mdSidenav', '$state',
