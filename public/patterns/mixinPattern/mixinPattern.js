88d0684e505b5aa3f9033fe41a93a4cb94b7448d
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-08 20:40:51 -0500

    added a page and controller for the mixin pattern

---
+++ public/patterns/mixinPattern/mixinPattern.js
@@ -0,0 +1,11 @@
+/* global angular */
+
+angular.module('jsPatternsDemo')
+.controller('MixinPatternCtrl', 
+    [
+        function() {
+            var vm = this;
+              
+        }
+    ]
+);
