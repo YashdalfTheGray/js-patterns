857f6c486af9d121935068ff231682480810d8b8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-09 21:15:49 -0500

    added a page and controller for the decorator pattern

---
+++ public/patterns/decoratorPattern/decoratorPattern.js
@@ -0,0 +1,11 @@
+/* global angular */
+
+angular.module('jsPatternsDemo')
+.controller('DecoratorPatternCtrl', 
+    [
+        function() {
+            var vm = this;
+            
+        }
+    ]
+);
