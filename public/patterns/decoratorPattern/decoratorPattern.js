c2d96cdb1217dfc756fadf7cded1544bad5302f5
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-10 22:29:44 -0500

    created a laptop contructor to be decorated

--- public/patterns/decoratorPattern/decoratorPattern.js
+++ public/patterns/decoratorPattern/decoratorPattern.js
@@ -6,6 +6,33 @@ angular.module('jsPatternsDemo')
         function() {
             var vm = this;
             
+            function Laptop() {
+                this.processor = {
+                    clockFrequency: 2.0,
+                    units: 'GHz'
+                };
+                this.memory = {
+                    capacity: 4,
+                    units: 'GB'
+                };
+                this.storage = {
+                    size: 500,
+                    units: 'GB',
+                    type: '7200RPM HDD'
+                };
+                this.display = {
+                    size: 13.3,
+                    resolution: '1920x1200'
+                }
+                this.video = {
+                    memory: 2,
+                    units: 'GB',
+                    type: 'Integrated'
+                }
+                this.cost = function() {
+                    return 700.00;
+                }
+            }
         }
     ]
 );
