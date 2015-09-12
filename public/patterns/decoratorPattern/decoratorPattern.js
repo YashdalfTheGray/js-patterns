faa56331e9bf4a1fc3deeb3afe96f5906e27d64e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-12 17:53:52 -0500

    updated the laptop object and added decorators

--- public/patterns/decoratorPattern/decoratorPattern.js
+++ public/patterns/decoratorPattern/decoratorPattern.js
@@ -7,31 +7,84 @@ angular.module('jsPatternsDemo')
             var vm = this;
             
             function Laptop() {
-                this.processor = {
-                    clockFrequency: 2.0,
-                    units: 'GHz'
+                this.processor = function() {
+                    return 2.0
                 };
-                this.memory = {
-                    capacity: 4,
-                    units: 'GB'
+                this.memory = function() {
+                    return 4;
                 };
-                this.storage = {
-                    size: 500,
-                    units: 'GB',
-                    type: '7200RPM HDD'
+                this.storage = function() {
+                    return 128;
                 };
-                this.display = {
-                    size: 13.3,
-                    resolution: '1920x1200'
+                this.videoMemory = function() {
+                    return 2;
                 };
-                this.video = {
-                    memory: 2,
-                    units: 'GB',
-                    type: 'Integrated'
+                this.displaySize = function() {
+                    return 13.3;
                 };
                 this.cost = function() {
                     return 700.00;
                 };
+            }
+
+            vm.upgrades = {
+                processor: function(latop) {
+                    var p = laptop.processor();
+                    var c = laptop.cost();
+
+                    laptop.processor = function() {
+                        return p + 0.5;
+                    };
+
+                    laptop.cost = function() {
+                        return c + 200;
+                    };
+                },
+                memory: function(laptop) {
+                    var m = laptop.memory();
+                    var c = laptop.cost();
+
+                    laptop.memory = function() {
+                        return m + 4;
+                    };
+
+                    laptop.cost = function() {
+                        return c + 100;
+                    };
+                },
+                storage: function(laptop) {
+                    var s = laptop.storage();
+                    var c = laptop.cost();
+
+                    laptop.storage = function() {
+                        return s + 384;
+                    };
+                    laptop.cost = function() {
+                        return c + 400;
+                    };
+                },
+                videoMemory: function() {
+                    var v = laptop.videoMemory();
+                    var c = laptop.cost();
+
+                    laptop.videoMemory = function() {
+                        return v + 2;
+                    };
+                    laptop.cost = function() {
+                        return c + 250;
+                    }
+                },
+                displaySize: function() {
+                    var d = laptop.displaySize();
+                    var c = cost();
+
+                    laptop.displaySize = function() {
+                        return d + 2.3;
+                    }
+                    laptop.cost = function() {
+                        return c + 300;
+                    }
+                }
             }
         }
     ]
