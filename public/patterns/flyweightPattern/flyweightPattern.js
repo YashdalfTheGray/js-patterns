1585a3e1005db3f8e353eeb0a1bdc1b768b189f2
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-13 12:38:29 -0500

    added a page and controller for the flyweight pattern

---
+++ public/patterns/flyweightPattern/flyweightPattern.js
@@ -0,0 +1,13 @@
+/* global angular */
+/* global _ */
+
+angular.module('jsPatternsDemo')
+.controller('FlyweightPatternCtrl', 
+    [
+        function() {
+            "use strict";
+            var vm = this;
+              
+        }
+    ]
+);
