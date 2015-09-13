5f7efffd7135fa7f3cbb5f53163cdf47a3bb877a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-12 20:02:01 -0500

    added a gulpfile for linting and cleaning npm modules

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    os = require('os'),
+    chalk = require('chalk'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish-ex'),
+    del = require('del');
+
+
+gulp.task('default', ['usage']);
+
+gulp.task('usage', function() {
+    var usageLines = [
+        '',
+        '',
+        chalk.green('usage'),
+        '\tDisplay this help page.',
+        '',
+        chalk.green('jshint'),
+        '\tRun jshint on the spec and the js folder under src.',
+        '',
+        chalk.green('clean:modules'),
+        '\tDeletes the npm_modules directory.',
+        '\t' + chalk.magenta('NOTE:') + ' ' + chalk.green('npm install') +
+        ' will be required before running the app.',
+        ''
+    ];
+    gutil.log(usageLines.join(os.EOL));
+});
+
+gulp.task('jshint', function() {
+    return gulp.src([
+        'public/**/*.js',
+        'gulpfile.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('clean:modules', function() {
+    return del([
+        'node_modules'
+    ]);
+});
