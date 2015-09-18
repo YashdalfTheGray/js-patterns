44af037cc636b260b34441776d49bd17f6da43e1
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-09-17 20:07:38 -0500

    added a demo case for the visitor pattern

--- public/patterns/visitorPattern/visitorPattern.js
+++ public/patterns/visitorPattern/visitorPattern.js
@@ -4,7 +4,51 @@ angular.module('jsPatternsDemo')
 .controller('VisitorPatternCtrl',
     [
         function() {
-    
+            var vm = this;
+
+            function Employee(name, salary, vacation) {
+                var self = this;
+
+                this.accept = function(visitor) {
+                    visitor.visit(self);
+                };
+
+                this.getName = function() {
+                    return name;
+                };
+
+                this.setName = function(value) {
+                    name = value;
+                };
+
+                this.getSalary = function() {
+                    return salary;
+                };
+
+                this.setSalary = function(value) {
+                    salary = value;
+                };
+
+                this.getVacation = function() {
+                    return vacation;
+                };
+
+                this.setVacation = function(value) {
+                    vacation = value;
+                };
+            }
+
+            function ExtraSalary() {
+                this.visit = function(employee) {
+                    employee.setSalary(employee.getSalary() * 1.2);
+                };
+            }
+
+            function ExtraVacation() {
+                this.visit = function(employee) {
+                    employee.setVacation(employee.getVacation + 5);
+                };
+            }
         }
     ]
 );
