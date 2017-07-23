7e4359f8cf65714dc7044cd8911257769fda342d
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-07-22 22:48:19 -0700

    added markdown because that was missing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function requireAll(requireContext) {
 
 require('./app');
 require('./app.css');
+require('./markdown.css');
 
 requireAll(require.context('./home', true, /\.js$/));
 requireAll(require.context('./patterns', true, /\.js$/));
