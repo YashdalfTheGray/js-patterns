32970202d6667d8a773f8713041b784885711a9a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-07-23 20:17:53 -0700

    there was only one file in that folder, pull it in by name

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ function requireAll(requireContext) {
 require('./app');
 require('./app.css');
 require('./markdown.css');
+require('./home/home.jsx');
 
-requireAll(require.context('./home', true, /\.js$/));
 requireAll(require.context('./patterns', true, /\.js$/));
