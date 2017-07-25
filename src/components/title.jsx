3cbe385615390fa5e6f312dc14cad391dcc1f975
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-07-24 18:46:57 -0700

    extracted out the title component into a different folder

---
+++ src/components/title.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Title() {
+    return (
+        <h2 style={{ padding: '8px' }}>
+            Please select a pattern from the sidebar.
+        </h2>
+    );
+}
