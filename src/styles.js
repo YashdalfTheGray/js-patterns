e218651c192b1b489d39dbf4ad5aebaae00a1b78
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-09-16 01:01:57 -0400

    added some flex styles, need to figure out a better way to do this

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+export default {
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    flexColumm: {
+        display: 'flex',
+        flexDirection: 'column'
+    }
+};
